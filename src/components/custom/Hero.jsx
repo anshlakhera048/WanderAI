517ae82fe2daaa91401be38db46504eb2b3aa53e
Ansh Lakhera <lakheraansh@gmail.com>
2025-06-05 12:31:21 +0530

    responsive changes

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,30 +1,36 @@
-import React from 'react'
-import { Button } from '../ui/button'
-import { Link } from 'react-router-dom'
-import Footer from '@/view-trip/[tripid]/components/Footer'
+import React from "react";
+import { Button } from "../ui/button";
+import { Link } from "react-router-dom";
+import Footer from "@/view-trip/[tripid]/components/Footer";
 
 function Hero() {
   return (
-    <div className='flex flex-col items-center mx-60 gap-9'>
-        <h1
-            className='font-extrabold text-[50px] text-center mt-12'
-        ><span className='text-[#fe8b00]'>Discover Everything you are planning your Trip for with</span> <span className='text-[#007b9a]'><br/>AI ITINERARY PLANNER</span>
-        </h1>
-        <p className='text-center text-[22px] text-gray-500'>
-        Plan your perfect trip with the power of AI. Smart, fast, and personalized travel planning — made simple.<br/>
+    <div className="flex flex-col items-center px-5 sm:px-10 md:px-20 lg:px-32 xl:px-48 max-w-screen-xl mx-auto gap-9">
+      <h1 className="text-center font-extrabold text-3xl sm:text-4xl lg:text-5xl leading-tight mt-12">
+        <span className="text-[#fe8b00]">
+          Discover everything you need to plan your trip
+        </span>
+        <span className="text-[#007b9a] block mt-2">
+          with AI Itinerary Planner
+        </span>
+      </h1>
+
+      <p className="text-center text-base sm:text-lg lg:text-[22px] text-gray-500">
+        Plan your perfect trip with the power of AI. Smart, fast, and
+        personalized travel planning — made simple.
+        <br />
         Smart, fast, and personalized travel planning — made simple.
-        </p>
-        
-        <Link to={'/create-trip'}>
-         <Button className=''>Get Started</Button>
-        </Link>
+      </p>
 
-        <img src='/landing.png' className='h-[450px]'/>
+      <Link to={"/create-trip"}>
+        <Button className="">Get Started</Button>
+      </Link>
 
-        <Footer/>
-       
+      <img src="/landing.png" className="w-full max-w-md sm:max-w-xl h-auto" />
+
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Hero 
+export default Hero;
